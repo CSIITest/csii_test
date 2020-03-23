227f8f782260584755c3ab6e0237fd4744112dcc
luchuangshuai@163.com <luchuangshuai@163.com>
2020-03-23 21:13:02 +0800

    qwe

---
+++ CSII_totals/src/test/java/com/csii/TestDemo.java
@@ -0,0 +1,18 @@
+package com.csii;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println(Integer.parseInt("0"));
+		
+		String string = "01";
+		
+		string = "000";
+		
+		System.out.println(string);
+		
+		
+	}
+}
